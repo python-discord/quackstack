0f547fac75f09c6ec90563df39257b6731356b52
vcokltfre <vcokltfre@gmail.com>
2021-04-01 15:06:47 +0100

    remove optional from the option models instead of random generation like that which is needlessly complex, im going to implement random generation by not passing any options

--- src/models.py
+++ src/models.py
@@ -3,21 +3,21 @@ from typing import Optional
 
 
 class PartOption(BaseModel):
-    colour: Optional[int]
-    lightness: Optional[float]
-    dark: Optional[bool]
-    hue: Optional[float]
+    colour: int
+    lightness: float
+    dark: bool
+    hue: float
 
 
 class Accessories(BaseModel):
-    hat: Optional[str]
-    outfit: Optional[str]
-    equipment: Optional[str]
+    hat: str
+    outfit: str
+    equipment: str
 
 
 class DuckRequest(BaseModel):
-    body: Optional[PartOption]
-    wing: Optional[PartOption]
-    eye: Optional[PartOption]
-    beak: Optional[PartOption]
-    accessories: Optional[Accessories]
+    body: PartOption
+    wing: PartOption
+    eye: PartOption
+    beak: PartOption
+    accessories: Accessories
