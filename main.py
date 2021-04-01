8fd965fb9296bc9500cc36c119917b82be2305c1
vcokltfre <vcokltfre@gmail.com>
2021-04-01 16:27:11 +0100

    add main api file and routes

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
 from fastapi import FastAPI
 from typing import Optional
+from hashlib import sha1
+from pathlib import Path
+from json import dumps
+from time import time
+from os import getenv
+
+from starlette.responses import StreamingResponse
 
 from src.models import DuckRequest
+from src.generator import DuckBuilder
 
+
+CACHE = Path(getenv("LOCATION", "./ducks"))
 
 app = FastAPI(docs_url=None)
 
+def dicthash(data: dict):
+    return sha1(dumps(data).encode()).hexdigest()
+
 @app.get("/duck")
-async def get_duck(duck: Optional[DuckRequest]):
+async def get_duck(duck: Optional[DuckRequest] = None):
     """Create a new duck."""
-    pass  # TODO: Implement this endpoint.
+
+    if duck:
+        dh = dicthash(duck.dict())
+        file = CACHE / f"{dh}.png"
+
+        if not file.exists():
+            DuckBuilder.generate(duck).save(file)
+    else:
+        dh = sha1(str(time).encode()).hexdigest()
+        file = CACHE / f"{dh}.png"
+
+        DuckBuilder.generate().save(file)
+
+    return {"file": f"/static/{dh}.png"}
 
 @app.get("/details")
 async def get_details():
     """Get details about accessories which can be used to build ducks."""
-    pass  # TODO: Implement this endpoint.
+
+    return {
+        "hats": list(DuckBuilder.hats.keys()),
+        "outfits": list(DuckBuilder.outfits.keys()),
+        "equipments": list(DuckBuilder.equipments.keys()),
+    }
