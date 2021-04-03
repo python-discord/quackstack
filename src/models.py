b06fe6460ee2e9ff33f64e714033e000c2697942
Joe Banks <joseph@josephbanks.me>
2021-04-03 14:37:01 +0100

    PEP8 fixes in models.py

--- src/models.py
+++ src/models.py
@@ -1,20 +1,27 @@
-from pydantic import BaseModel
 from typing import Optional
+
+from pydantic import BaseModel
 
 
 class PartOption(BaseModel):
+    """Valid options for a ducky component."""
+
     r: int
     g: int
     b: int
 
 
 class Accessories(BaseModel):
+    """Valid accessories for a duck."""
+
     hat: Optional[str]
     outfit: Optional[str]
     equipment: Optional[str]
 
 
 class DuckRequest(BaseModel):
+    """A request for a ducky generation."""
+
     body: PartOption
     wing: PartOption
     eye: PartOption
