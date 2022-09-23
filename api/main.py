8458c1d17dd74dcc183a84808374db3e1c909daf
Chris Lovering <chris.lovering.95@gmail.com>
2022-09-23 21:23:04 +0100

    Split random ducks from ducks with specific options
    This is mostly needed so that we do not have request bodies being used in GET requests, as this isn't supported in many HTTP client libraries (such as HTTPX)

--- api/main.py
+++ api/main.py
@@ -5,10 +5,11 @@ from pathlib import Path
 from time import time
 from typing import Optional, Union
 
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Request
 from fastapi.exceptions import HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from starlette.datastructures import URL
 
 from api.models import DuckRequest, DuckResponse, DuckyDetails, ManDuckDetails, ManDuckRequest, ManDuckVariations
 from quackstack import DuckBuilder, ManDuckBuilder
@@ -34,53 +35,76 @@ def dicthash(data: dict) -> str:
     return sha1(dumps(data).encode()).hexdigest()
 
 
-@app.get("/duck", response_model=DuckResponse)
-async def get_duck(duck: Optional[DuckRequest] = None, seed: Optional[int] = None) -> DuckResponse:
-    """Create a new duck."""
-    if duck:
-        dh = dicthash(duck.dict())
-        file = CACHE / f"{dh}.png"
-
-        if not file.exists():
-            try:
-                DuckBuilder().generate(options=duck.dict()).image.save(file)
-            except ValueError as e:
-                raise HTTPException(400, e.args[0])
-            except KeyError as e:
-                raise HTTPException(400, f"Invalid configuration option provided: '{e.args[0]}'")
-    else:
-        dh = sha1(str(time()).encode()).hexdigest()
-        file = CACHE / f"{dh}.png"
-
-        DuckBuilder(seed).generate().image.save(file)
-
-    return DuckResponse(file=f"/static/{dh}.png")
+def make_file_path(dh: str, request_url: URL) -> str:
+    """Build a file path from dict_hash and a URL."""
+    fqdn = str(request_url).removesuffix(request_url.path)
+    return f"{fqdn}/static/{dh}.png"
 
 
-@app.get("/manduck", response_model=DuckResponse)
-async def get_man_duck(manduck: Optional[ManDuckRequest] = None, seed: Optional[int] = None) -> DuckResponse:
-    """Create a new man_duck."""
-    if manduck:
-        dh = dicthash(manduck.dict())
-        file = CACHE / f"{dh}.png"
+@app.get("/duck", response_model=DuckResponse, status_code=201)
+async def get_duck(request: Request, response: Response, seed: Optional[int] = None) -> DuckResponse:
+    """Create a new random duck, with an optional seed."""
+    dh = sha1(str(time()).encode()).hexdigest()
+    file = CACHE / f"{dh}.png"
 
-        if not file.exists():
-            try:
-                ducky = ManDuckBuilder().generate(options=manduck.dict())
-            except ValueError as e:
-                raise HTTPException(400, e.args[0])
-            except KeyError as e:
-                raise HTTPException(400, f"Invalid configuration option provided: '{e.args[0]}'")
-            ducky.image.save(CACHE / f"{dh}.png")
+    DuckBuilder(seed).generate().image.save(file)
 
-    else:
-        dh = sha1(str(time()).encode()).hexdigest()
+    file_path = make_file_path(dh, request.url)
+    response.headers["Location"] = file_path
+    return DuckResponse(file=file_path)
 
-        ducky = DuckBuilder(seed).generate()
-        ducky = ManDuckBuilder(seed).generate(ducky=ducky)
+
+@app.post("/duck", response_model=DuckResponse, status_code=201)
+async def post_duck(request: Request, response: Response, duck: DuckRequest = None) -> DuckResponse:
+    """Create a new duck with a given set of options."""
+    dh = dicthash(duck.dict())
+    file = CACHE / f"{dh}.png"
+
+    if not file.exists():
+        try:
+            DuckBuilder().generate(options=duck.dict()).image.save(file)
+        except ValueError as e:
+            raise HTTPException(400, e.args[0])
+        except KeyError as e:
+            raise HTTPException(400, f"Invalid configuration option provided: '{e.args[0]}'")
+
+    file_path = make_file_path(dh, request.url)
+    response.headers["Location"] = file_path
+    return DuckResponse(file=file_path)
+
+
+@app.get("/manduck", response_model=DuckResponse, status_code=201)
+async def get_man_duck(request: Request, response: Response, seed: Optional[int] = None) -> DuckResponse:
+    """Create a new man_duck, with an optional seed."""
+    dh = sha1(str(time()).encode()).hexdigest()
+
+    ducky = DuckBuilder(seed).generate()
+    ducky = ManDuckBuilder(seed).generate(ducky=ducky)
+    ducky.image.save(CACHE / f"{dh}.png")
+
+    file_path = make_file_path(dh, request.url)
+    response.headers["Location"] = file_path
+    return DuckResponse(file=file_path)
+
+
+@app.post("/manduck", response_model=DuckResponse, status_code=201)
+async def post_man_duck(request: Request, response: Response, manduck: ManDuckRequest) -> DuckResponse:
+    """Create a new man_duck with a given set of options."""
+    dh = dicthash(manduck.dict())
+    file = CACHE / f"{dh}.png"
+
+    if not file.exists():
+        try:
+            ducky = ManDuckBuilder().generate(options=manduck.dict())
+        except ValueError as e:
+            raise HTTPException(400, e.args[0])
+        except KeyError as e:
+            raise HTTPException(400, f"Invalid configuration option provided: '{e.args[0]}'")
         ducky.image.save(CACHE / f"{dh}.png")
 
-    return DuckResponse(file=f"/static/{dh}.png")
+    file_path = make_file_path(dh, request.url)
+    response.headers["Location"] = file_path
+    return DuckResponse(file=file_path)
 
 
 @app.get("/details/{type}", response_model=Union[ManDuckDetails, DuckyDetails])
